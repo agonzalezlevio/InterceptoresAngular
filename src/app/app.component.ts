47844b413f3694c156b279158269ebe4acbd58e7
axlfrost <ale.gonzalezlevio@gmail.com>
2019-12-04 20:12:51 -0300

    Implementación servicio básico de obtención de usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interceptorApp';
+
+
+  constructor( private usuarioService: UsuarioService) {
+    this.usuarioService.obtenerUsuarios().subscribe( resp => {
+      console.log(resp);
+    }, (respError) => {
+      console.log(respError)
+    });
+  }
+
 }
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+
+  constructor(private http: HttpClient) { }
+
+
+  public obtenerUsuarios() {
+      // Parámetros
+      let params = new HttpParams();
+      params = params.append('page', '2');
+      // Headers
+      const headers = new HttpHeaders({
+      'x-token-usuario': 'ADS65ADAD6AD5AS6DAD5AS6DA'
+    });
+      return this.http.get(`https://reqres.xin/api/users`, { params, headers }).pipe(
+        // Filtrar la información
+        map( (res: any) => {
+          return res.data;
+        }),
+        // Errores
+        catchError( this.manejaError )
+        );
+  }
+
+
+  public manejaError(error: HttpErrorResponse) {
+    console.log('Sucedió un error');
+    console.warn(error);
+    return throwError('Error personalizado');
+  }
+
+}
+
+
